6350c97ee49924bafce46b173c075867841ba450
nyxflower <nyx0flower@gmail.com>
2020-03-28 22:24:58 -0400

    Change data structue

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import pickle
 from torch.nn import Parameter, Module
 
 
@@ -26,16 +27,31 @@ def load_graph(pt_file_path='./sample_graph.pt'):
         - data.n_node_type: number of node types == (1 or 2)
         - data.n_edge: number of edges
         - data.n_edge_type: number of edge types
-
         - data.node_type: (source_node_type, target_node_type)
-        - data.edge_index: torch.Tensor, int, shape (2, n_edge)
+
+        - data.edge_index: [list of] torch.Tensor, int, shape (2, n_edge), [indexed by edge type]
             [0, :] : source node index
             [1, :] : target node index
-        - data.edge_type: torch.Tensor, int, shape (n_edge,)
-        - data.edge_weight: torch.Tensor, float, shape (n_edge,)
+        - data.edge_type: None or list of torch.Tensor, int, shape (n_edge,), indexed by edge type
+        - data.edge_weight: None or list of torch.Tensor, float, shape (n_edge,)
 
         - data.source_node_idx_to_id: dict {idx : id}
         - data.target_node_idx_to_id: dict {idx : id}
     """
 
     return torch.load(pt_file_path)
+
+
+def load_node_idx_to_id_dict(pkl_file_path='./data/pose/map.pkl'):
+    """
+    Parameters:
+    -----------
+    The path of index maps in the dataset directory
+
+    Returns:
+    --------
+    a dictionary of map from node index to entity id/name
+    """
+    with open(pkl_file_path, 'rb') as f:
+        out = pickle.load(f)
+    return out
