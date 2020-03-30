cd4277ff7691091e9b560761550fb3a0e66b540f
nyxflower <nyx0flower@gmail.com>
2020-03-30 01:08:13 -0400

    Change drug index

--- data/process.py
+++ data/process.py
@@ -6,6 +6,20 @@ with open("data/tipexp_data.pkl", 'rb') as f:
 
 from torch_geometric.data import Data
 
+dd_edge_index = torch.cat([data.train_idx, data.test_idx], dim=1)
+dd_edge_type = torch.cat([data.train_et, data.test_et])
+
+index = []
+ttt = []
+for i in range(len(data.train_range)):
+    print(i)
+    idx = torch.cat([data.train_idx[:, data.train_range[i][0]: data.train_range[i][1]],
+                     data.test_idx[:, data.test_range[i][0]: data.test_range[i][1]]], dim=1)
+    tt = torch.cat([data.train_et[data.train_range[i][0]: data.train_range[i][1]],
+                    data.test_et[data.test_range[i][0]: data.test_range[i][1]]])
+    index.append(idx)
+    ttt.append(tt)
+
 drug = Data.from_dict({
     'n_node': data.n_drug,
     'n_node_type': 1,
@@ -13,8 +27,8 @@ drug = Data.from_dict({
     'n_edge_type': data.n_et,
     'node_type': ('drug', 'drug'),
 
-    'edge_index': data.dd_edge_index,
-    'edge_type': data.dd_edge_type,
+    'edge_index': index,
+    'edge_type': ttt,
     'edge_weight': None
 })
 torch.save(drug, './data/pose/drug-drug.pt')
