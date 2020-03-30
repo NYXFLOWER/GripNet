350f2109d5560ecdb6aa2711f72ea45cc3cb1322
nyxflower <nyx0flower@gmail.com>
2020-03-30 01:09:19 -0400

    Add node sp feat and neg samp

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import pickle
+from sklearn import metrics
 from torch.nn import Parameter, Module
 
 
@@ -12,6 +13,26 @@ EPS = 1e-13
 def normalize(input):
     norm_square = (input ** 2).sum(dim=1)
     return input / torch.sqrt(norm_square.view(-1, 1))
+
+
+def sparse_id(n):
+    idx = [[i for i in range(n)], [i for i in range(n)]]
+    val = [1 for i in range(n)]
+    i = torch.LongTensor(idx)
+    v = torch.FloatTensor(val)
+    shape = (n, n)
+
+    return torch.sparse.FloatTensor(i, v, torch.Size(shape))
+
+
+def auprc_auroc_ap(target_tensor, score_tensor):
+    y = target_tensor.detach().cpu().numpy()
+    pred = score_tensor.detach().cpu().numpy()
+    auroc, ap = metrics.roc_auc_score(y, pred), metrics.average_precision_score(y, pred)
+    y, xx, _ = metrics.ranking.precision_recall_curve(y, pred)
+    auprc = metrics.ranking.auc(xx, y)
+
+    return auprc, auroc, ap
 
 
 def load_graph(pt_file_path='./sample_graph.pt'):
@@ -55,3 +76,27 @@ def load_node_idx_to_id_dict(pkl_file_path='./data/pose/map.pkl'):
     with open(pkl_file_path, 'rb') as f:
         out = pickle.load(f)
     return out
+
+
+def negative_sampling(pos_edge_index, num_nodes):
+    idx = (pos_edge_index[0] * num_nodes + pos_edge_index[1])
+    idx = idx.to(torch.device('cpu'))
+
+    perm = torch.tensor(np.random.choice(num_nodes**2, idx.size(0)))
+    mask = torch.from_numpy(np.isin(perm, idx).astype(np.uint8))
+    rest = mask.nonzero().view(-1)
+    while rest.numel() > 0:  # pragma: no cover
+        tmp = torch.tensor(np.random.choice(num_nodes**2, rest.size(0)))
+        mask = torch.from_numpy(np.isin(tmp, idx).astype(np.uint8))
+        perm[rest] = tmp
+        rest = mask.nonzero().view(-1)
+
+    row, col = perm / num_nodes, perm % num_nodes
+    return torch.stack([row, col], dim=0).long().to(pos_edge_index.device)
+
+
+def typed_negative_sampling(pos_edge_index, num_nodes, range_list):
+    tmp = []
+    for start, end in range_list:
+        tmp.append(negative_sampling(pos_edge_index[:, start: end], num_nodes))
+    return torch.cat(tmp, dim=1)
