b172427bf9552b55c0f9163b7e35a8d410b82f75
nyxflower <nyx0flower@gmail.com>
2020-04-07 17:29:53 -0400

    Add multi-class inner product decoder

--- src/decoder.py
+++ src/decoder.py
@@ -1,9 +1,9 @@
 from src.utils import *
 
 
-class multiInnerProductDecoder(Module):
+class multiRelaInnerProductDecoder(Module):
     def __init__(self, in_dim, num_et):
-        super(multiInnerProductDecoder, self).__init__()
+        super(multiRelaInnerProductDecoder, self).__init__()
         self.num_et = num_et
         self.in_dim = in_dim
         self.weight = Parameter(torch.Tensor(num_et, in_dim))
@@ -16,3 +16,20 @@ class multiInnerProductDecoder(Module):
 
     def reset_parameters(self):
         self.weight.data.normal_(std=1/np.sqrt(self.in_dim))
+
+
+class multiClassInnerProductDecoder(Module):
+    def __init__(self, in_dim, num_class):
+        super(multiClassInnerProductDecoder, self).__init__()
+        self.num_class = num_class
+        self.in_dim = in_dim
+        self.weight = Parameter(torch.Tensor(num_class, in_dim))
+
+        self.reset_parameters()
+
+    def forward(self, z, node_list, node_label, sigmoid=True):
+        value = (z[node_list] * self.weight[node_label]).sum(dim=1)
+        return torch.sigmoid(value) if sigmoid else value
+
+    def reset_parameters(self):
+        self.weight.data.normal_()
