7523e1ca8e5d50136ff662870784cdb2f8d9e3b9
nyxflower <nyx0flower@gmail.com>
2020-04-07 12:17:52 -0400

    Add gd gg edge types to graph and sampling

--- data/process.py
+++ data/process.py
@@ -1,10 +1,14 @@
+from torch_geometric.data import Data
+from data.utils import process_edge_multirelational
 import pickle
 import torch
 
+
+# ###################################
+# MIP-pose dataset
+# ###################################
 with open("data/tipexp_data.pkl", 'rb') as f:
     data = pickle.load(f)
-
-from torch_geometric.data import Data
 
 dd_edge_index = torch.cat([data.train_idx, data.test_idx], dim=1)
 dd_edge_type = torch.cat([data.train_et, data.test_et])
@@ -67,4 +71,59 @@ idx_to_id_dict = {
 
 with open('./data/pose/pose_map.pkl', 'wb') as f:
     pickle.dump(idx_to_id_dict, f)
+
+
+# ###################################
+# comb-pose dataset
+# ###################################
+# load data
+dd = torch.load('data/pose/drug-drug.pt')
+gd = torch.load('data/pose/gene-drug.pt')
+gg = torch.load('data/pose/gene-gene.pt')
+
+# add dd graph
+index = dd.edge_index.copy()
+ttt = dd.edge_type.copy()
+
+# add gg graph
+tmp = gg.edge_index + dd.n_node
+index.append(tmp)
+tmp = torch.tensor([len(index)] * gg.n_edge)
+ttt.append(tmp)
+
+# add gd graph
+tmp = gd.edge_index
+tmp[0] = tmp[0] + dd.n_node
+index.append(tmp)
+tmp = torch.tensor([len(index)] * gd.n_edge)
+ttt.append(tmp)
+
+# train and test set
+values = process_edge_multirelational(index, p=0.9)
+
+# construction
+data = Data.from_dict({
+    'n_node': dd.n_node + gg.n_node,
+    'n_node_type': 2,
+    'n_edge': dd.n_edge + gd.n_edge + gg.n_edge,
+    'n_edge_type': dd.n_edge_type + gd.n_edge_type + gg.n_edge_type,
+    'node_type': [('drug', 'drug'), ('gene', 'drug'), ('gene', 'gene')],
+
+    'edge_index': index,
+    'edge_type': ttt,
+    'edge_weight': None,
+
+    'train_idx': values[0],
+    'train_et': values[1],
+    'train_range': values[2],
+    'test_idx': values[3],
+    'test_et': values[4],
+    'test_range': values[5],
+
+    'description': 'reindex node [drug]+[gene]'
+})
+
+torch.save(data, './data/pose_comb_all.pt')
+
+
 
