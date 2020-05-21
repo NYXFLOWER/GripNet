90a6b82b90e091f44102d076b4ac4c2d0c68958f
nyxflower <nyx0flower@gmail.com>
2020-05-21 13:33:12 -0400

    Add pose-1 results on auroc

--- out/eva.py
+++ out/eva.py
@@ -17,33 +17,49 @@ auroc_rgcn_org = [rgcn_org.test_out[i][1] for i in range(100)]
 rgcn = torch.load('out/multi-rela/pose_rgcn_all/100-64-32-16-16-0.01-record.pt')
 auroc_rgcn = [rgcn.test_out[i][1] for i in range(100)]
 
-plt.figure(1, figsize=(20, 10))
+fig = plt.figure(1, figsize=(10, 3))
 
-plt.subplot(231)
+plt.subplot(121)
 x = np.array(range(100), dtype=int) + 1
-plt.plot(x, auroc_mip, label='MIP', color='y')
+plt.plot(x, auroc_mip, label='GripNet', color='y')
 plt.plot(x, auroc_dmt_org, label='DistMult', color=(1, 0, 1))
 plt.plot(x, auroc_rgcn_org, label='RGCN', color=(0, 1, 1))
 plt.plot(x, auroc_dmt, label='DistMult++', color='r')
 plt.plot(x, auroc_rgcn, label='RGCN++', color='b')
 plt.title('AUROC by epoch')
-plt.legend()
+plt.ylim((0.45, 0.95))
+# plt.legend()
 plt.grid()
 
-plt.subplot(232)
+ax = plt.subplot(122)
 tmp = [i for i in (x*21) if i < 1000]
-plt.plot(tmp, auroc_mip[:len(tmp)], label='MIP', color='y')
+plt.plot(tmp, auroc_mip[:len(tmp)], label='$GripNet$', color='y')
 tmp = [i for i in (x*11.7) if i < 1000]
-plt.plot(tmp, auroc_dmt[:len(tmp)], label='DistMult++', color='r')
-plt.plot(tmp, auroc_dmt_org[:len(tmp)], label='DistMult', color=(1, 0, 1))
+plt.plot(tmp, auroc_dmt[:len(tmp)], label='$DistMult^{TNG}$', color='r')
+plt.plot(tmp, auroc_dmt_org[:len(tmp)], label='$DistMult$', color=(1, 0, 1))
 
 tmp = [i for i in (x*59.6) if i < 1000]
-plt.plot(tmp, auroc_rgcn[:len(tmp)], label='RGCN++', color='b')
-plt.plot(tmp, auroc_rgcn_org[:len(tmp)], label='RGCN', color=(0, 1, 1))
+plt.plot(tmp, auroc_rgcn[:len(tmp)], label='$RGCN^{TNG}$', color='b')
+plt.plot(tmp, auroc_rgcn_org[:len(tmp)], label='$RGCN$', color=(0, 1, 1))
 
-plt.title('AUROC by time')
-plt.legend()
+plt.title('AUROC by time (s)')
+plt.ylim((0.45, 0.95))
+# plt.legend()
 plt.grid()
+
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width, box.height])
+
+# Put a legend to the right of the current axis
+ax.legend(loc='center left', bbox_to_anchor=(1.02, 0.7))
+# plt.subplot(133)
+# plt.scatter(19.3, 12.22, label='GripNet', color='y', marker='P')
+# plt.scatter(59.6, 10.92, label='RGCN', color=(1, 0, 1), marker='D')
+# plt.scatter(59.6, 10.94, label='RGCN++', color='b', marker='*')
+# plt.scatter(11.7, 9.3, label='DistMult', color=(0, 1, 1), marker='o')
+# plt.scatter(11.7, 9.31, label='DistMult++', color='r', marker='1')
+# plt.title('Peak GPU memory usage (GB) by time (s)')
 
 plt.savefig('./fig/pose_auroc1.png')
 plt.show()
