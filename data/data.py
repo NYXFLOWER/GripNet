a3b1d1a93b4e07179d72e034368ca2173e2cca5f
nyxflower <nyx0flower@gmail.com>
2020-05-21 13:33:24 -0400

    Update data.py

--- data/data.py
+++ data/data.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import torch
 from torch_geometric.data import Data
-from data.utils import process_node_multilabel
 
-n = 2
-l = ['business', 'location', 'organization']
+bb = torch.from_numpy(pd.read_csv('data/freebase/book_lo_business/index_separated/link_book-book_1.dat', usecols=[0, 1], sep='\t', header=None).to_numpy().T)
+bp = torch.from_numpy(pd.read_csv('data/freebase/book_lo_business/index_separated/link_book-business_4.dat', usecols=[0, 1], sep='\t', header=None).to_numpy().T)
+pp = torch.from_numpy(pd.read_csv('data/freebase/book_lo_business/index_separated/link_business-business_5.dat', usecols=[0, 1], sep='\t', header=None).to_numpy().T)
+bq = torch.from_numpy(pd.read_csv('data/freebase/book_lo_business/index_separated/link_book-location&organization_2.dat', usecols=[0, 1], sep='\t', header=None).to_numpy().T)
+qq = torch.from_numpy(pd.read_csv('data/freebase/book_lo_business/index_separated/link_location&organization_3.dat', usecols=[0, 1], sep='\t', header=None).to_numpy().T)
 
-bb = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/book-book.dat'.format(l[n]), sep='\t', header=None).to_numpy().T)
-bp = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/book-{}.dat'.format(l[n], l[n]), sep='\t', header=None).to_numpy().T)
-pp = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/{}-{}.dat'.format(l[n], l[n], l[n]), sep='\t', header=None).to_numpy().T)
 
-train = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/label.dat.train'.format(l[n]), sep='\t', header=None).to_numpy().T)
-test = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/label.dat.test'.format(l[n]), sep='\t', header=None).to_numpy().T)
+# train = torch.from_numpy(pd.read_csv('data/freebase_org/book_{}/index_separated/label.dat.train'.format(l[n]), sep='\t', header=None).to_numpy().T)
+# test = torch.from_numpy(pd.read_csv('data/freebase/book_{}/index_separated/label.dat.test'.format(l[n]), sep='\t', header=None).to_numpy().T)
 
 
 data = Data()
@@ -22,22 +21,20 @@ data.pp_edge_idx = pp
 data.n_a_node = (max(bb.max(), bp[0].max()) + 1).tolist()
 data.n_p_node = (max(pp.max(), bp[1].max()) + 1).tolist()
 
-data.n_a_type = (train[1].max() + 1).tolist()
+data.n_a_type = 8
 
 data.n_aa_edge = bb.shape[1]
 data.n_pp_edge = pp.shape[1]
 data.n_pa_edge = bp.shape[1]
 
-idx, sam = [], []
-for nn in range(data.n_a_type):
-    tr = train[0][train[1] == nn]
-    te = test[0][test[1] == nn]
-    idx.append(torch.cat((tr, te)))
-    sam.append(idx[nn].__len__())
+torch.save(data, 'datasets-freebase/business.pt')
 
 
-data.train_node_idx, data.train_node_class, data.train_range, data.test_node_idx, data.test_node_class, data.test_range = process_node_multilabel(idx)
-data.test_sample = [(nn[1] - nn[0]).tolist() for nn in data.test_range]
-data.train_sample = [(nn[1] - nn[0]).tolist() for nn in data.train_range]
+data.qa_edge_idx = torch.tensor([bq[1].tolist(), bq[0].tolist()])
+data.qq_edge_idx = qq
+data.n_q_node = (max(qq.max(), bq[1].max()) + 1).tolist()
+data.n_qq_edge = qq.shape[1]
+data.n_qa_edge = bq.shape[1]
 
-torch.save(data, 'datasets-freebase/book-{}.pt'.format(l[n]))
+torch.save(data, 'datasets-freebase/all.pt')
+
