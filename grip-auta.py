4f1ec3c1dc9e2ad62a487a3821d86b8a43814d26
nyxflower <nyx0flower@gmail.com>
2020-05-28 14:24:09 -0400

    Update grip-auta.py

--- grip-auta.py
+++ grip-auta.py
@@ -3,6 +3,7 @@ from src.decoder import multiClassInnerProductDecoder
 from torch_geometric.data import Data
 import sys, time, os
 import pandas as pd
+from data.utils import process_data_multiclass
 
 torch.manual_seed(1111)
 np.random.seed(1111)
@@ -11,10 +12,15 @@ np.random.seed(1111)
 # ###################################
 # data processing
 # ###################################
+lll = int(sys.argv[-1])
 data = torch.load('datasets-auta-org/auta.pt')
+train = torch.from_numpy(pd.read_csv('datasets-auta/train_test_split/label.dat.train_{}'.format(lll), sep='\t', header=None).to_numpy().T)
+test = torch.from_numpy(pd.read_csv('datasets-auta/train_test_split/label.dat.test_{}'.format(lll), sep='\t', header=None).to_numpy().T)
+data.train_node_idx, data.train_node_class, data.train_range = process_data_multiclass(train, data.n_a_type)
+data.test_node_idx, data.test_node_class, data.test_range = process_data_multiclass(test, data.n_a_type)
 
-# output path
-out_dir = './out/auta/'
+# output path`
+out_dir = './nout/auta/auta-2l1l-add/'.format()
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
@@ -23,7 +29,8 @@ data.a_feat = sparse_id(data.n_a_node)
 data.p_feat = sparse_id(data.n_p_node)
 data.aa_edge_weight = torch.ones(data.n_aa_edge)
 data.pp_edge_weight = torch.ones(data.n_pp_edge)
-data.n_node_per_type = [(i[1] - i[0]).data.tolist() for i in data.test_range]
+data.n_node_per_type = [(i[1] - i[0]) for i in data.test_range]
+data.n_node_per_type_train = [(i[1] - i[0]) for i in data.train_range]
 
 
 # output dictionary
@@ -55,16 +62,19 @@ class Model(Module):
 
 # hyper-parameter setting
 pp_nhids_gcn = [int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
-pa_gcn = int(sys.argv[5])
-pa_out = int(sys.argv[6])
-aa_nhids_gcn = [pa_gcn + pa_out, int(sys.argv[7]), int(sys.argv[8])]
+# pa_gcn = int(sys.argv[5])
+pa_out = [int(sys.argv[5]), int(sys.argv[6])]
+# aa_nhids_gcn = [pa_gcn + pa_out, int(sys.argv[7]), int(sys.argv[8])]
+aa_nhids_gcn = [pa_out[-1], int(sys.argv[8])]
 learning_rate = 0.01
 
+# homoGraph(pp_nhids_gcn, start_graph=True, in_dim=data.n_p_node),
+# pp_nhids_gcn[0] = data.n_p_node
 # model init
 model = Model(
-    homoGraph(data.n_p_node, pp_nhids_gcn, start_graph=True),
-    interGraph(sum(pp_nhids_gcn), pa_gcn, data.n_a_node, target_feat_dim=pa_out),
-    homoGraph(pa_out, aa_nhids_gcn),
+    homoGraph(pp_nhids_gcn, start_graph=True, in_dim=data.n_p_node),
+    interGraph(sum(pp_nhids_gcn), pa_out[0], data.n_a_node, target_feat_dim=pa_out[-1]),
+    homoGraph(aa_nhids_gcn),
     multiClassInnerProductDecoder(sum(aa_nhids_gcn), data.n_a_type)
 ).to(device)
 
@@ -75,14 +85,14 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # ###################################
 # Train and Test
 # ###################################
-# @profile
+@profile
 def train(epoch):
     model.train()
     optimizer.zero_grad()
 
-    z = model.pp(data.p_feat, data.pp_edge_idx, edge_weight=data.pp_edge_weight)
-    z = model.pa(z, data.pa_edge_idx)
-    z = model.aa(z, data.aa_edge_idx, edge_weight=data.aa_edge_weight)
+    z = model.pp(data.p_feat, data.pp_edge_idx, edge_weight=data.pp_edge_weight, if_catout=True)
+    z = model.pa(z, data.pa_edge_idx, if_relu=True, mod='add')
+    z = model.aa(z, data.aa_edge_idx, edge_weight=data.aa_edge_weight, if_catout=True)
 
     score = model.mcip(z, data.train_node_idx)
     pred = torch.argmax(score, dim=1)
@@ -124,13 +134,13 @@ for epoch in range(EPOCH_NUM):
 
     micro, macro = test(z)
 
-    print('{:3d}   loss:{:0.4f}   micro:{:0.4f}   macro:{:0.4f}    time:{:0.1f}\n'
+    print('{:3d}   loss:{:0.4f}   micro:{:0.4f}   macro:{:0.4f}    time:{:0.2f}\n'
           .format(epoch, loss.tolist(), micro, macro, (time.time() - time_begin)))
 
     out.test_out[epoch] = np.array([micro, macro])
 
 # model name
-name = '-{}-{}-{}-{}-{}'.format(pp_nhids_gcn, pa_gcn, pa_out, pa_out, aa_nhids_gcn, learning_rate)
+name = '-{}-{}-{}'.format(pp_nhids_gcn, pa_out, aa_nhids_gcn)
 
 if device == 'cuda':
     data = data.to('cpu')
@@ -146,4 +156,5 @@ with open(out_dir + str(EPOCH_NUM) + name + '.txt', 'w') as f:
 
 print('The trained model and the result record have been saved!')
 
-torch.save(z, out_dir + str(EPOCH_NUM) + name + 'weight.pt')
+torch.save(z, out_dir + str(EPOCH_NUM) + name + '-weight.pt')
+
